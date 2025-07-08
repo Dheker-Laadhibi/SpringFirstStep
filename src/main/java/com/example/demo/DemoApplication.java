495e7d8a12f15b674a837516248a831bb0405696
Dheker-Laadhibi <dhekerlaadhibii@gmail.com>
2025-07-08 15:48:43 +0100

    control

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+//This class will handle HTTP requests and return data, not HTML pages
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+
+@GetMapping
+    public  String helloworld (){return ("hello world");}
 }
